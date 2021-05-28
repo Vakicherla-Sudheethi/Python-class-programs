8f97e74d33591b4b52390ba17bea513dbf2640a2
Vakicherla.Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2021-05-28 11:25:17 +0530

    Add files via upload

---
+++ lists1.py
@@ -0,0 +1,6 @@
+#creating a list of branches offered by college
+branch_names=["cse","it","mech","ece","agri"]
+#printing
+print(branch_names)
+#printing the elements through indexes
+print(branch_names[1])
---
+++ lists2.py
@@ -0,0 +1,11 @@
+colleges=["aec","jntuk","iit",1,2,3,"kkd","surampalem","kharagpur"]
+print(colleges)
+#data type of colleges
+print("data type of colleges",type(colleges))
+#modification or change the list name is possible
+colleges[1]="pragathi"
+print(colleges)
+#access list elements by element by using for loop
+for i in colleges:
+    print(i)
+    
