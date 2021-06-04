26dc4be44f660ac11cc7234819d3214757fb14cf
Vakicherla.Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2021-06-04 18:10:52 +0530

    Add files via upload

---
+++ slicingOfTuples.py
@@ -0,0 +1,24 @@
+#tuple creation
+branch=("cse","it","ece","agri","mech","pt")
+#slicing of tuples
+print(branch)
+#prints starting element
+print(branch[1])
+#prints n to n-1 ie 2 to 4
+print(branch[2:5])
+#gives 0-5
+print(branch[:5])
+#prints 3 to end
+print(branch[3:])
+#prints entire tuple
+print(branch[:])
+#starting to step count 2
+print(branch[::2])
+#having negative step count
+print(branch[::-2])
+#all elements from left to right
+print(branch[::1])
+#prints right to left in reverse order
+print(branch[::-1])
+#prints frm left to right in reverse order
+print(branch[-5:-2])
