e3a9e76ead525c6c01b4a0c417a0b278744fcc9b
Vakicherla.Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2021-06-21 15:29:53 +0530

    Add files via upload

---
+++ fun3.py
@@ -0,0 +1,17 @@
+#function with return expression
+def greetings():#function call
+    print("Hi")
+    return("good afternoon")
+#call the function
+#variable=function_name()
+result=greetings()
+print(result)
+"""output:Hi
+good afternoon"""
+#calling and printing in single line
+print("second way")
+print(greetings())
+"""output:
+second way
+Hi
+good afternoon"""
