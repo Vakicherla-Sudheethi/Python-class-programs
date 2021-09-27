8b28ea2112fcb66c7fe4e2e960448f53136e9e84
Vakicherla.Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2021-09-27 19:53:15 +0530

    Add files via upload

---
+++ guvi 2.py
@@ -0,0 +1,16 @@
+#lists
+#addition of elements in a list
+#creation of empty list
+list=[]
+print(list)
+#adding of elements
+list.append(1)
+list.append(2)
+list.append(4)
+print(list)
+list.append((1,2,3))
+#inserting elements into list by indexes
+list2=[1,3,4,5,6]
+list2.insert(1,2)
+list2.extend([1,2,3,4,5])
+print(list2)
